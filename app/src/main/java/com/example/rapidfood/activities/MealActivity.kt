9bcf410342cf805d152b2f499ef819624564af8f
NISHKARSH SAXENA <94742536+nishkarsh800@users.noreply.github.com>
2023-09-12 22:48:23 +0530

    Impl Food details feature and MealViewmodel

--- app/src/main/java/com/example/rapidfood/activities/MealActivity.kt
+++ app/src/main/java/com/example/rapidfood/activities/MealActivity.kt
@@ -2,25 +2,49 @@ package com.example.rapidfood.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
-import com.example.rapidfood.R
 import com.example.rapidfood.databinding.ActivityMealBinding
 import com.example.rapidfood.HomeFragment
+import com.example.rapidfood.R
+import com.example.rapidfood.dataclasses.Meal
+import com.example.rapidfood.viewModel.MealViewModel
 
 class MealActivity : AppCompatActivity() {
     private lateinit var mealId:String
     private lateinit var mealName:String
     private lateinit var mealThumb:String
     private lateinit var binding: ActivityMealBinding
+    private lateinit var mealMvvm: MealViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMealBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mealMvvm = ViewModelProviders.of(this)[MealViewModel::class.java]
         getMealInformtionFromIntent()
 
         setInformationInViews()
+
+        mealMvvm.getMealDetail(mealId)
+        observeMealDeatilsLiveData()
     }
+
+    private fun observeMealDeatilsLiveData() {
+        mealMvvm.observeMealDetailsLiveData().observe(this,object :Observer<Meal>{
+            override fun onChanged(value: Meal) {
+               val meal = value
+
+                binding.tvCategories.text = "Category:${meal!!.strCategory}"
+                binding.tvArea.text = "Area:${meal.strArea}"
+                binding.tvInstructionsSteps.text = meal.strInstructions
+            }
+
+        })
+    }
+
 
     private fun setInformationInViews() {
         Glide.with(applicationContext)
@@ -28,6 +52,8 @@ class MealActivity : AppCompatActivity() {
             .into(binding.imgMealDetail)
 
         binding.collapsingToolbar.title = mealName
+        binding.collapsingToolbar.setCollapsedTitleTextColor(resources.getColor(R.color.white))
+        binding.collapsingToolbar.setExpandedTitleColor(resources.getColor(R.color.white))
     }
 
     private fun getMealInformtionFromIntent() {
