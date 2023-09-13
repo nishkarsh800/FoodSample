1b8d17468881122081113e97ba85bdb362e64bb4
NISHKARSH SAXENA <94742536+nishkarsh800@users.noreply.github.com>
2023-09-13 22:21:42 +0530

    Impl YT link and minor fixes

--- app/src/main/java/com/example/rapidfood/activities/MealActivity.kt
+++ app/src/main/java/com/example/rapidfood/activities/MealActivity.kt
@@ -1,9 +1,11 @@
 package com.example.rapidfood.activities
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
 import com.example.rapidfood.databinding.ActivityMealBinding
@@ -13,10 +15,11 @@ import com.example.rapidfood.dataclasses.Meal
 import com.example.rapidfood.viewModel.MealViewModel
 
 class MealActivity : AppCompatActivity() {
-    private lateinit var mealId:String
-    private lateinit var mealName:String
-    private lateinit var mealThumb:String
+    private lateinit var mealId: String
+    private lateinit var mealName: String
+    private lateinit var mealThumb: String
     private lateinit var binding: ActivityMealBinding
+    private lateinit var youtubeLink : String
     private lateinit var mealMvvm: MealViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,18 +31,32 @@ class MealActivity : AppCompatActivity() {
 
         setInformationInViews()
 
+        loadingCase()
+
         mealMvvm.getMealDetail(mealId)
         observeMealDeatilsLiveData()
+
+        onYoutubeImageClick()
+    }
+
+    private fun onYoutubeImageClick() {
+        binding.imgYoutube.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeLink))
+            startActivity(intent)
+        }
     }
 
     private fun observeMealDeatilsLiveData() {
-        mealMvvm.observeMealDetailsLiveData().observe(this,object :Observer<Meal>{
+        mealMvvm.observeMealDetailsLiveData().observe(this, object : Observer<Meal> {
             override fun onChanged(value: Meal) {
-               val meal = value
+                onResponseCase()
+                val meal = value
 
                 binding.tvCategories.text = "Category:${meal!!.strCategory}"
                 binding.tvArea.text = "Area:${meal.strArea}"
                 binding.tvInstructionsSteps.text = meal.strInstructions
+
+                youtubeLink = meal.strYoutube
             }
 
         })
@@ -61,5 +78,24 @@ class MealActivity : AppCompatActivity() {
         mealId = intent.getStringExtra(HomeFragment.MEAL_ID)!!
         mealName = intent.getStringExtra(HomeFragment.MEAL_NAME)!!
         mealThumb = intent.getStringExtra(HomeFragment.MEAL_THUMB)!!
+    }
+
+    private fun loadingCase() {
+        binding.progressBar.visibility = View.VISIBLE
+        binding.btnAddToFav.visibility = View.INVISIBLE
+        binding.tvInstructions.visibility = View.INVISIBLE
+        binding.tvCategories.visibility = View.INVISIBLE
+        binding.tvArea.visibility = View.INVISIBLE
+        binding.imgYoutube.visibility = View.INVISIBLE
+    }
+
+    private fun onResponseCase() {
+
+        binding.progressBar.visibility = View.INVISIBLE
+        binding.btnAddToFav.visibility = View.VISIBLE
+        binding.tvInstructions.visibility = View.VISIBLE
+        binding.tvCategories.visibility = View.VISIBLE
+        binding.tvArea.visibility = View.VISIBLE
+        binding.imgYoutube.visibility = View.VISIBLE
     }
 }
