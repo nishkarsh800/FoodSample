58dfd447fb63970d69560597e1b34e6f77620f0c
NISHKARSH SAXENA <94742536+nishkarsh800@users.noreply.github.com>
2023-09-25 23:30:30 +0530

    Minor Fixes

--- app/src/main/java/com/example/rapidfood/fragments/CategoriesFragment.kt
+++ app/src/main/java/com/example/rapidfood/fragments/CategoriesFragment.kt
@@ -14,7 +14,9 @@ private const val ARG_PARAM2 = "param2"
 /**
  * A simple [Fragment] subclass.
  * Use the [CategoriesFragment.newInstance] factory method to
- * create an instance of this fragCategoriesFragment : Fragment() {
+ * create an instance of this fragment.
+ */
+class CategoriesFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -54,4 +56,4 @@ private const val ARG_PARAM2 = "param2"
                 }
             }
     }
-}**/
+}
