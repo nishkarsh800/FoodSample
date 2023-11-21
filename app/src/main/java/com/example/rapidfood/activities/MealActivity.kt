c2ef91b09d0c435ab16297c1a50cde94f06f8012
NISHKARSH SAXENA <94742536+nishkarsh800@users.noreply.github.com>
2023-11-21 22:34:23 +0530

    SetUp of RoomDB

--- app/src/main/java/com/example/rapidfood/activities/MealActivity.kt
+++ app/src/main/java/com/example/rapidfood/activities/MealActivity.kt
@@ -56,7 +56,7 @@ class MealActivity : AppCompatActivity() {
             binding.tvArea.text = "Area:${meal.strArea}"
             binding.tvInstructionsSteps.text = meal.strInstructions
 
-            youtubeLink = meal.strYoutube
+            youtubeLink = meal.strYoutube.toString()
         }
     }
 
---
+++ app/src/main/java/com/example/rapidfood/database/MealTypeConverter.kt
@@ -0,0 +1,20 @@
+package com.example.rapidfood.database
+
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+
+@TypeConverters
+class MealTypeConverter {
+    @TypeConverter
+    fun fromAnyToString(attribute:Any?):String{
+        if(attribute == null)
+            return ""
+        return attribute as String
+    }
+    @TypeConverter
+    fun fromStringToAny(attribute: String?):Any{
+        if(attribute == null)
+            return ""
+        return attribute as Any
+    }
+}
